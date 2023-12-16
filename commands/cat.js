33b1a8a86914d5e92e84aa78d085b50ff8040063
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2023-12-16 11:27:22 +0000

    fixed cats

--- commands/cat.js
+++ commands/cat.js
@@ -1,5 +1,5 @@
 const url ='https://cataas.com/cat?json=true';
-const start='https://cataas.com';
+const start='https://cataas.com/cat/';
 const fetch = require('node-fetch')
 module.exports= {
     async execute(message,client){
@@ -10,7 +10,7 @@ module.exports= {
         const embed ={
             title: "Cat",
             url:start,//it's the same
-            image:{url:`${start+data.url}`},
+            image: {url:`${start+data._id}`},
             colour:16133127,
             footer:{text:`using cataas.com`},
         }
